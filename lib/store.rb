65638bf8e06c38ca95bb397a24926b36fa79b21f
Joey Kishiuchi <josephkishiuchi@outlook.com>
2020-05-26 01:18:40 +0000

    Completed exercise 9

--- lib/store.rb
+++ lib/store.rb
@@ -1,12 +1,20 @@
 class Store < ActiveRecord::Base
   has_many :employees
-  validates :name, presence: true ,length: {minimum: 3}
-  validates :annual_revenue, presence: true ,numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :name ,length: {minimum: 3}
+  validates :annual_revenue ,numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validate :must_carry_mens_or_womens_apparel
+
+  before_destroy :stop_destruction_for_stores_with_employees
 
   def must_carry_mens_or_womens_apparel
     unless (mens_apparel == true || womens_apparel == true)
       errors.add(:mens_apparel, "Store must carry mens and/or womens apparel")
     end
   end
+  private 
+    def stop_destruction_for_stores_with_employees 
+      if self.employees.count > 0
+        false
+      end
+    end
 end
