578e1fe86a71a34055fd6c46ebbca135b143cf9f
Joey Kishiuchi <josephkishiuchi@outlook.com>
2020-05-26 00:19:12 +0000

    Completed exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,20 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.first
+@store2 = Store.find_by(id: 2)
+@store4 = Store.find_by(id: 4)
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "John", last_name: "Smith", hourly_rate: 20)
+@store1.employees.create(first_name: "Jake", last_name: "Grewal", hourly_rate: 50)
+@store1.employees.create(first_name: "Don", last_name: "Hardman", hourly_rate: 90)
+
+@store2.employees.create(first_name: "Gale", last_name: "Jimathin", hourly_rate: 100)
+@store2.employees.create(first_name: "Stacey", last_name: "Keating", hourly_rate: 30)
+@store2.employees.create(first_name: "Ben", last_name: "Stiller", hourly_rate: 70)
+@store2.employees.create(first_name: "Kyle", last_name: "Johnson", hourly_rate: 60)
+
+employees = Employee.all
+puts employees.count
